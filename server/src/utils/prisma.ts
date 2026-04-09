8e27b2d919f47c41e33f7380f21edaa45f8428af
Usuario <contato@zap.com>
2026-04-09 15:28:53 -0300

    fix(backend): downgrade prisma to stable v5.21 to support seamless paas deployment

--- server/src/utils/prisma.ts
+++ server/src/utils/prisma.ts
@@ -3,11 +3,7 @@ import { PrismaClient } from '@prisma/client';
 // Evitar instanciar múltiplos clientes Prisma em ambiente de desenvolvimento (Hot Reloading)
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  datasources: {
-    db: { url: process.env.DATABASE_URL }
-  }
-});
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
